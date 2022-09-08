4f4419ce9480005280af7d18a45ebc6bfaffbecc
samuel <samuelogunmola4@gmail.com>
2022-09-08 17:55:52 +0100

    new update to 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Single line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+
+}
