ad576eecf6a20c0daf0ceae68aa52c7e4a67ebc4
samuel <samuelogunmola4@gmail.com>
2022-09-08 19:38:49 +0100

    hopefully the last update to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -Function
+ * main -Entry point
  * Return: 0 (Succes)
  */
 int main(void)
 {
-puts(\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
 }
