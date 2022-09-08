7fc36c3d6a4005ae0ff5a518e114fb5607244d7b
samuel <samuelogunmola4@gmail.com>
2022-09-08 19:16:14 +0100

    hopefully the last update to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main -Function
  * Return: 0 (Succes)
  */
-int main(void)
+int main(0)
 {
-puts("/"Programming is like building a multilingual puzzle/");
+puts("Programming is like building a multilingual puzzle");
 
 return (0);
 }
