bda9304ec643bb8eb4cb2a68c910f32be78b7dc4
samuel <samuelogunmola4@gmail.com>
2022-09-10 18:13:26 +0100

    updating the 101-quote file. the last one maybe

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
