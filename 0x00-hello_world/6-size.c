13c2a01907f611d89c6fcfacebd1f52567b4128a
samuel <samuelogunmola4@gmail.com>
2022-09-10 17:00:59 +0100

    Adding 6-size.c to the list

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+main(void)
+{
+clrscr();
+printf("short int is %2d bytes \n", sizeof(short int));
+printf("int is %2d bytes \n", sizeof(int));
+printf("int * is %2d bytes \n", sizeof(int *));
+printf("long int is %2d bytes \n", sizeof(long int));
+printf("ong int * is %2d bytes \n", sizeof(long int *));
+printf("signed int is %2d bytes \n", sizeof(signed int));
+printf("unsigned int is %2d bytes \n", sizeof(unsigned int));
+printf("\n");
+printf("        float is %2d bytes \n", sizeof(float));
+printf("      float * is %2d bytes \n", sizeof(float *));
+printf("       double is %2d bytes \n", sizeof(double));
+printf("     double * is %2d bytes \n", sizeof(double *));
+printf("  long double is %2d bytes \n", sizeof(long double));
+printf("\n");
+printf("  signed char is %2d bytes \n", sizeof(signed char));
+printf("         char is %2d bytes \n", sizeof(char));
+printf("       char * is %2d bytes \n", sizeof(char *));
+printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
+getch();
+}
