ced772a78cbbc032e9410e9a3457caccff84d0ff
samuel <samuelogunmola4@gmail.com>
2022-09-11 07:57:25 +0100

    Hopefully the last update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful"' - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
