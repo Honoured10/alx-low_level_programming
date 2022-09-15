733f7166651f1da49db136a1da37b39bb912bf70
samuel <samuelogunmola4@gmail.com>
2022-09-15 14:23:49 +0100

    Added 10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - draws a triangle using multiple terminal lines.
+ * @size: the height of the triangle.
+ */
+void print_triangle(int size)
+{
+int i, j;
+
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = size; j >= 1; j--)
+{
+if (i < j)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
