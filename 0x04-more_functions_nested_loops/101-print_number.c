c41631d39d9349091eddbffedc3b760e52854c7d
samuel <samuelogunmola4@gmail.com>
2022-09-15 14:31:47 +0100

    Added 101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_number - prints a number
+ * @n: Input number
+ */
+
+void print_number(int n)
+{
+	long len, sam, i, temp, expo;
+
+	sam = n;
+	expo = len =  1;
+/*Check negatives*/
+	if (sam < 0)
+	{
+		sam *= -1;
+		_putchar('-');
+	}
+
+/**/
+	temp = sam;
+	while (temp >= 10)
+	{
+		len++;
+		temp /= 10;
+	}
+
+/*Create Exponent*/
+	for (i = 1; i < len; i++)
+		expo *= 10;
+/*Main */
+	while (expo > 1)
+	{
+		_putchar((sam / expo) % 10 + '0');
+		expo /= 10;
+	}
+	_putchar(sam % 10 + '0');
+}
