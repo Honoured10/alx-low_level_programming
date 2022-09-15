84e655856cff4b1f190fa71bc2a39a2a1c45d2fa
samuel <samuelogunmola4@gmail.com>
2022-09-15 09:21:40 +0100

    added the header file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - checks for digits 1-9
+ * @c : character to check.
+ * Return: 1 if c is a digit
+ * Return 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
