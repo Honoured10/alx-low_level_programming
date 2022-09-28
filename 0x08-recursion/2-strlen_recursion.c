6819b1ad881d47229fbc3764247b2132e934432d
samuel <samuelogunmola4@gmail.com>
2022-09-28 15:38:46 +0100

    fixed error

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,12 @@
-#include "main.h"
 /**
- * _strlen_recursion -prints the length of a string.
- * @s: string to print
- * Returns: lenght of the string.
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to count
+ * Return: length of the string
  */
-
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
-if (*s == '\o')
+if (*s == '\0')
 return (0);
 s++;
-else
-return (1 + _strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s));
 }
