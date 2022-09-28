916d7211044a8fccaf49d38e09ae89ed795d433a
samuel <samuelogunmola4@gmail.com>
2022-09-28 16:43:38 +0100

    fixed error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,12 +2,12 @@
 /**
  * _print_rev_recursion - prints a string in reverse.
  * @s: string to print
- * Return: 
+ * Return:
  */
 
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
+if (*s)
 {
 _print_rev_recursion(s + 1);
 _putchar(*s);
