71d44aa42ba44e1dc01b1f4801f068747d4e9e94
samuel <samuelogunmola4@gmail.com>
2022-09-28 22:51:42 +0100

    Fixed output issue

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,11 @@
 
 int checker(int n, int base)
 {
-if (n * n < base)
-return (-1);
-else if
-(n * n == base)
+if (n * n == base)
 return (n);
+else if
+(n * n < base)
+return (-1);
 else
 return (checker(n + 1, base));
 }
