53dd3572448de77f160fa31080b9e4c32e7aac4a
samuel <samuelogunmola4@gmail.com>
2022-09-28 22:05:49 +0100

    Fixed betty issues

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _pow_recursion - Raise a number to the power of another.
- * @s: the numbers to raise power
+ * @x: base number
+ * @y: power of
  * Return: the value of x raised to the power of y.
  */
 
@@ -9,6 +10,8 @@ int _pow_recursion(int x, int y)
 {
 if (y < 0)
 return (-1);
+else if (y <= 0)
+return (1);
 else
-return (_pow_recursion(x^y));
+return (_pow_recursion(x, y - 1));
 }
