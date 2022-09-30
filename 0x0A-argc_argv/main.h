44b8b8238808acb4933b9a8a3ee2da0e7c843cec
samuel <samuelogunmola4@gmail.com>
2022-09-30 22:32:26 +0100

    fixed error in main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int check_num(char *str);
 int checker(int argc, int i, unsigned int j, char *argv[]);
+
+#endif /* MAIN_H */
