32ffb43d8d0655d1b2e801093dcd45af19aca067
samuel <samuelogunmola4@gmail.com>
2022-09-30 22:00:52 +0100

    Adding 1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
