d5d25e1a6f67317c86e6b162a83f23aa0dc8388e
Honoured10 <samuelogunmola4@gmail.com>
2022-10-13 17:58:33 +0100

    Fixed errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
-  * print_name - prints a name
-  * @name: input string
-  * @f: function pointer casted to char pointer
-  */
+* print_name - prints a name
+* @name: name to print
+* @f: pointer to the printing function
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
