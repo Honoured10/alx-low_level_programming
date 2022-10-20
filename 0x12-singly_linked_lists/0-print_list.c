89e133ab5e0eb47a8273a5d69b77cf46f5d48aee
Honoured10 <samuelogunmola4@gmail.com>
2022-10-20 16:37:19 +0100

    Fixed checker error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,29 @@
+
+
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list
- * @h: Head of the list
- *
- * Return: Numbers of elements
- **/
+* print_list - prints all the elements of a list
+*
+* @h:pointer to the list
+*
+*Return: the number of nodes
+*/
+
 size_t print_list(const list_t *h)
 {
-	int i;
+const list_t *cursor = h;
+size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
+while (cursor != NULL)
+{
+if (cursor->str != NULL)
+printf("[%d] %s\n", cursor->len, cursor->str);
+else
+printf("[0] (nil)\n");
+count += 1;
+cursor = cursor->next;
+}
 
-	for (i = 0; h; i++)
-	{
-		printf("[%u] %s\n", (h->str != NULL) ? h->len : 0,
-				(h->str != NULL) ? h->str : "(nil)");
-		h = h->next;
-	}
-
-	return (i);
+return (count);
 }
