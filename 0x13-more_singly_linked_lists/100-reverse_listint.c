15b3a8f8f992075b997e5af2fce14a7fbf786715
Honoured10 <samuelogunmola4@gmail.com>
2022-10-25 15:26:14 +0100

    Fixed checker error

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,26 @@
 #include "lists.h"
+
 /**
-  * reverse_listint - define function
-  * @head: describe argument
-  * Return: what does it return?
-  */
+ * reverse_listint - Reverses a listint_t linked list
+ * @head: Head of the list
+ *
+ * Return: Tail of the list as head
+ **/
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev, *current, *next;
+
+	prev = next = NULL;
+	current = *head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+
+	return (*head);
 }
